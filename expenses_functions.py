a64cecbd3c76ab26cdaac1d83ec22906f325546c
camwise92 <cameroncarlisle1992@gmail.com>
2024-07-31 11:25:56 +0100

    Expense Tracker
    Developed from intro course. This is my first attempt to share across devices using github.

---
+++ expenses_functions.py
@@ -0,0 +1,73 @@
+"""
+Author: Cameron
+Description : Expense tracker
+Date: 26/07/2024
+
+"""
+import modules_for_tracker
+import json
+from datetime import datetime
+import os
+import sys
+location = os.path.dirname(os.path.abspath(sys.argv[0]))
+os.chdir(location)
+  
+def main():
+    print("Expense Tracker\n==============\n")
+    read_expenses_from_json()
+    enter_expenses()
+    modules_for_tracker.sum(expenses)
+    
+
+    
+
+expenses = {}
+expense_item_number = 0
+
+def enter_expenses():
+    while True:
+        global expense_item_number  
+
+        # expense, date(use format), category, amount, description
+       
+        name = input("Enter the name of the expense (or 0 to exit):")
+        if name == '0':
+            break
+        
+        date = modules_for_tracker.date_function()
+        category = modules_for_tracker.category_function()
+        amount = modules_for_tracker.amount_function()
+        description = input("Enter a description of the expense: ")
+
+        expenses.update({str(expense_item_number): {
+            "name": name,
+            "date": date,
+            "category": category,
+            "amount": amount,
+            "description": description}
+        })
+
+        expense_item_number+=1
+        write_expense_to_json()
+
+        
+def read_expenses_from_json():
+    try:
+        global expenses
+        global expense_item_number
+        with open("expense_tracker.json", "r") as file:
+            expenses = json.load(file)
+        last_expense_number = list(expenses)[-1]
+        expense_item_number = int(last_expense_number)+1
+    except FileNotFoundError:
+        print("New expense tracker created.\n")
+
+def write_expense_to_json():
+    with open("expense_tracker.json", "w") as file:
+        json.dump(expenses, file, indent=4)
+
+
+if __name__ == "__main__":
+    main()
+
+
---
+++ modules_for_tracker.py
@@ -0,0 +1,108 @@
+
+import json
+from datetime import datetime
+
+
+def amount_function():
+    while True:
+        try:
+            amount = input("Enter the amount: ")
+            if (amount.isalpha()) == False:
+                    break
+            else:
+                raise ValueError
+        except ValueError:
+            print("Invalid format. Please type numbers only.")
+    return amount
+        
+def category_function():
+    category_list = ["Store", "Online", "Bills", "Misc"]
+    while True:
+        try:
+            category = input("Enter the category (Store, Online, Bills, Misc ): ").title()
+            if category in category_list:
+                break
+            else:
+                raise ValueError
+        except ValueError:
+            print(f"Invalid category {category}. Please type from this list: Store, Online, Bills, Misc") 
+    return category
+
+def date_function():
+    while True:
+        try:
+            date = (input("Enter the date of expense (DD-MM-YY): "))
+            d = datetime.strptime(date, '%d-%m-%Y')
+            date = d
+            break
+        except ValueError:
+            print(f"Invalid date entered - {date}, please try again.")
+    
+    return(str(date).split()[0])
+
+    
+
+def write_expense_to_json():
+    with open("expense_tracker.json", "w") as file:
+        json.dump(expenses, file, indent=4)
+
+def read_expenses_from_json():
+    try:
+        global expenses
+        global expense_item_number
+        with open("expense_tracker.json", "r") as file:
+            expenses = json.load(file)
+        last_expense_number = list(expenses)[-1]
+        expense_item_number = int(last_expense_number)+1
+    except FileNotFoundError:
+        print("New expense tracker created.\n")
+
+def sum(data):
+    with open("expense_tracker.json", "r") as file:
+        data = json.load(file)
+    def find_key_values(data, target_key):
+        values = []
+        def recursive_search(d):
+            if isinstance(d, dict):
+                for key, value in d.items():
+                    if key == target_key:
+                        values.append(value)
+                    if isinstance(value, dict):
+                        recursive_search(value)
+                    elif isinstance(value, list):
+                        for item in value:
+                            if isinstance(item, dict):
+                                recursive_search(item)
+        recursive_search(data)
+        return values
+                
+
+    key_to_find = 'amount'
+    values = find_key_values(data,key_to_find)
+    values = [float(x) for x in values]
+    
+    sum = 0
+    for transaction in values:
+        sum += transaction
+
+    print(sum)
+
+
+    
+        
+'''
+want to write a function that will go into parts of dict and return sum of things:
+
+sum value of amount - and return it in currency almost done jusr nee d to return £ sign
+
+how many items are store, online, bills, misc could use same func as sum but have if statement 
+so if key to find equals bills do this and add bills to list
+elif its misc add to list etc
+then return how many was in each
+
+to do: think about a menu with options. 
+a clear function to cleqn up the comsole, 
+try except into sum functiom. test it and try to break it 
+give sum of values in certain month 
+
+go into the date and ask how may expenses were in a day, a year or a month - number of expenses and value'''
